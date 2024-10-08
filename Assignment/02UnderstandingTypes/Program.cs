a8b165acc5b3be881d263a5c3478c535cef9263f
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-08 18:16:48 -0400

    Add files via upload

---
+++ Assignment/02UnderstandingTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//sbyte
+Console.WriteLine($"size of sbyte : {sizeof(sbyte)}, min: {sbyte.MinValue}, max: {sbyte.MaxValue}");
+//byte
+Console.WriteLine($"size of byte : {sizeof(byte)}, min: {byte.MinValue}, max: {byte.MaxValue}");
+//short
+Console.WriteLine($"size of short : {sizeof(short)}, min: {short.MinValue}, max: {short.MaxValue}");
+//ushort
+Console.WriteLine($"size of ushort : {sizeof(ushort)}, min: {ushort.MinValue}, max: {ushort.MaxValue}");
+//int
+Console.WriteLine($"size of int : {sizeof(int)}, min: {int.MinValue}, max: {int.MaxValue}");
+//uint
+Console.WriteLine($"size of uint : {sizeof(uint)}, min: {uint.MinValue}, max: {uint.MaxValue}");
+//long
+Console.WriteLine($"size of long : {sizeof(long)}, min: {long.MinValue}, max: {long.MaxValue}");
+//ulong
+Console.WriteLine($"size of ulong : {sizeof(ulong)}, min: {ulong.MinValue}, max: {ulong.MaxValue}");
+//float
+Console.WriteLine($"size of float : {sizeof(float)}, min: {float.MinValue}, max: {float.MaxValue}");
+//double
+Console.WriteLine($"size of double : {sizeof(double)}, min: {double.MinValue}, max: {double.MaxValue}");
+//decimal
+Console.WriteLine($"size of decimal : {sizeof(decimal)}, min: {decimal.MinValue}, max: {decimal.MaxValue}");
