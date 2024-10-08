3ab74850c4b180ce79622198114900ad7c197c6e
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-08 18:17:39 -0400

    Add files via upload

---
+++ Assignment/04ControlFlowAndConvertTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 0;
+while (i < 8)
+{
+    i++;
+    if (i == 5)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+    
+}
---
+++ Assignment/05FizzBuzzis/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// The original code, the byte can only hold values from 0 to 255. When it comes to 256,
+// then it would be changed to 0 and do the for loop forever;
+
+int max = 500;
+
+// Check if max exceeds the byte range
+if (max > byte.MaxValue)
+{
+    Console.WriteLine("Warning: The value of 'max' exceeds the maximum allowed value.");
+}
+else
+{
+    for (byte i = 1; i < max; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
