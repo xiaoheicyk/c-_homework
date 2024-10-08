ed22aad4e76ef4b96dc5dae1497349d1c9a1d6ed
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-08 18:20:35 -0400

    Add files via upload

---
+++ Assignment/10Increament/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+for (int i = 1; i <= 4; i++)
+{
+    for (int j = 0; j <= 24; j+=i)
+    {
+        Console.Write(j);
+        if (j != 24)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine();
+}
