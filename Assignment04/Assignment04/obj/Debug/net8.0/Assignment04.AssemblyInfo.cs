cd841b30d7ad0f8fafee50ad65eaec4725d2e800
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-10 12:59:16 -0400

    i

--- Assignment04/Assignment04/obj/Debug/net8.0/Assignment04.AssemblyInfo.cs
+++ Assignment04/Assignment04/obj/Debug/net8.0/Assignment04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e61c2ec5e1928c4ab503f20c2509f9720f5318f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec81b7a6d30c51ab9ecb351cd0c528eb9737738")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
